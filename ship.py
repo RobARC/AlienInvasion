bcf3a14f17ba4d1ec9faf510353d499cdddd1dbb
RobARC <rrondonc@gmail.com>
2023-08-11 08:33:18 -0500

    Update ship.py adding method center ship

--- ship.py
+++ ship.py
@@ -1,10 +1,10 @@
 import pygame
 
 class Ship:
-    """ A claa to manage the ship."""
+    """ A class to manage the ship."""
 
     def __init__(self, ai_game):
-        """Initialize the ship and set it starting position."""
+        """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
         self.settings = ai_game.settings
@@ -35,3 +35,8 @@ class Ship:
         """Draw the ship and its current location"""
 
         self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        """Center the ship on the screen"""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
