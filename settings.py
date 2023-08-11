a3254c706f27817c2cefebad02b26142c9dd8a44
RobARC <rrondonc@gmail.com>
2023-08-11 08:35:39 -0500

    Update settings.py adding ship settings

--- settings.py
+++ settings.py
@@ -8,10 +8,10 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (135, 206, 235)
-        self.ship_speed = 1.5
+        
 
         # Bullets settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
@@ -23,4 +23,6 @@ class Settings:
         # fleet_direction of 1 represents right -1 represents left
         self.fleet_direction = 1
 
-
+        # Ships settings
+        self.ship_speed = 1.5
+        self.ship_limit = 3
