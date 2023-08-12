8c482370e96d82e8fff8cb7a734fcd3f52d267aa
RobARC <rrondonc@gmail.com>
2023-08-12 10:53:40 -0500

    updating setting.py add methods initialize dynamyc settings and increase spped

--- settings.py
+++ settings.py
@@ -26,3 +26,24 @@ class Settings:
         # Ships settings
         self.ship_speed = 1.5
         self.ship_limit = 3
+
+        # How quickly the game speeds up
+        self.speedup_scale = 1.5
+
+        self.initialize_dynamic_settings()
+
+    
+    def initialize_dynamic_settings(self):
+        """initialize settings that change throughput the game."""
+        self.ship_speed = 1.1
+        self.bullet_speed = 3.0
+        self.alien_speed = 1.0
+
+        # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+
+    def increase_speed(self):
+        """Increments the speed settings"""
+        self.ship_speed *= self.speedup_scale
+        self.bullet_speed *= self.speedup_scale
+        self.alien_speed *= self.speedup_scale
