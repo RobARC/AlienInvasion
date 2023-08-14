557de6c421f429adbd9be3cc18ca97c7229b58d1
RobARC <rrondonc@gmail.com>
2023-08-14 08:45:17 -0500

    Update file ship.py importing Sprite

--- ship.py
+++ ship.py
@@ -1,10 +1,12 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Ship:
+class Ship(Sprite):
     """ A class to manage the ship."""
 
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
+        super().__init__()
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
         self.settings = ai_game.settings
@@ -13,7 +15,7 @@ class Ship:
         self.image = pygame.image.load('images/SpaceShip3.png')
         self.rect = self.image.get_rect()
 
-        # Star each new ship at the botton center of the screen
+        # Start each new ship at the botton center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
         self.x = float(self.rect.x)
 
