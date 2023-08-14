eab4e2b99c03dc4927faa118181b117fa05eb4a5
RobARC <rrondonc@gmail.com>
2023-08-14 08:46:44 -0500

    Update file settings.py adding stars

--- settings.py
+++ settings.py
@@ -27,8 +27,14 @@ class Settings:
         self.ship_speed = 1.5
         self.ship_limit = 3
 
+        # Starts settings
+        self.stars_enable = 20
+
         # How quickly the game speeds up
         self.speedup_scale = 1.5
+
+        # How quickly the alient point values increase
+        self.score_scale = 1.5
 
         self.initialize_dynamic_settings()
 
@@ -38,6 +44,9 @@ class Settings:
         self.ship_speed = 1.1
         self.bullet_speed = 3.0
         self.alien_speed = 1.0
+        
+        # Scoring
+        self.alien_points = 50
 
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
@@ -47,3 +56,6 @@ class Settings:
         self.ship_speed *= self.speedup_scale
         self.bullet_speed *= self.speedup_scale
         self.alien_speed *= self.speedup_scale
+
+        self.alien_points += int(self.alien_points * self.score_scale)
+        
